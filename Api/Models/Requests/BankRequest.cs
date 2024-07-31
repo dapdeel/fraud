e13e44994c49c59fc032e62c36e2bc5331c660bd
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-07-31 20:29:18 +0100

    Update BankRequest.cs

--- Api/Models/Requests/BankRequest.cs
+++ Api/Models/Requests/BankRequest.cs
@@ -3,5 +3,5 @@ public class BankRequest
     public required string Name { get; set; }
     public required string Code { get; set; }
     public string? ImageUrl { get; set; }
-    public string Country {get;set;} = "NGN";
+    public string? Country {get;set;} = "NGN";
 }

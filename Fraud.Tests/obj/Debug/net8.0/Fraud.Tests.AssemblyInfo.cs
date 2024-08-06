ae5da9a234c84efed603661c90d4eb0e00180d88
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-08-06 15:29:01 +0100

    graph commit

--- Fraud.Tests/obj/Debug/net8.0/Fraud.Tests.AssemblyInfo.cs
+++ Fraud.Tests/obj/Debug/net8.0/Fraud.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fraud.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbcfe6a6dcfad89ba88fdc57f04ca6eca0329415")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e759b389598e0db215bbf553ad81e60119c9e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fraud.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fraud.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

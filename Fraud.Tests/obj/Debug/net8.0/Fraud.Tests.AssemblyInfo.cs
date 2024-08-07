715fb59d712ba8da8e0e001a5a2c051a8ce59b9f
Gbolahan Alao <alaogbolahan2000@gmail.com>
2024-08-07 03:37:54 +0100

    merge with qa

--- Fraud.Tests/obj/Debug/net8.0/Fraud.Tests.AssemblyInfo.cs
+++ Fraud.Tests/obj/Debug/net8.0/Fraud.Tests.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fraud.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e5ef8bb154fb02e85a810b6fc2776cb39f38ae")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7854f07e015de6159528651f19c41d4f7aaec9")]
->>>>>>> main
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5688c4ef58a8777fd31d75991d11e267dab6717")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fraud.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fraud.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

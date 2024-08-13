810ffc58a4476eec2f03a882019c1eac8f1a68ac
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-08-13 13:57:20 +0100

    introduced load balancer

--- Api/Controllers/PingController.cs
+++ Api/Controllers/PingController.cs
@@ -4,19 +4,48 @@ using Api.Interfaces;
 using Api.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Api.Services.Interfaces;
 
 [Route("api/[controller]")]
 [ApiController]
 [AllowAnonymous]
 public class PingController : ControllerBase
 {
-
+    private IGraphService _graphService;
+    public PingController(IGraphService graphService)
+    {
+        _graphService = graphService;
+    }
     [HttpGet("Start")]
     public IActionResult Start()
     {
         try
         {
 
+            return Ok(new ApiResponse<dynamic>
+            {
+                Status = "success",
+                Data = "Successful"
+            });
+        }
+        catch (ValidateErrorException Exception)
+        {
+            return BadRequest(new ApiResponse<dynamic>
+            {
+                Status = "ValidationError",
+                Error = new ApiError { Code = "", Details = Exception.Message },
+                Message = Exception.Message
+            });
+        }
+    }
+
+    [HttpGet("GraphTest")]
+    public IActionResult Test()
+    {
+        try
+        {
+            var connector = _graphService.connect();
+            var g = connector.traversal();
             return Ok(new ApiResponse<dynamic>
             {
                 Status = "success",

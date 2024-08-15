ad814b60da48f56afce0c04c3655c15dfdd4a932
Kayode Olaniyi <ifedamilola2009@gmail.com>
2024-08-15 12:37:21 +0100

    made _ traversal singleton for each request

--- Api/Graph/JanusGraphConnector.cs
+++ Api/Graph/JanusGraphConnector.cs
@@ -1,5 +1,6 @@
 using Gremlin.Net.Driver;
 using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Traversal;
 using JanusGraph.Net.IO.GraphSON;
 using static Gremlin.Net.Process.Traversal.AnonymousTraversalSource;
 
@@ -8,17 +9,23 @@ public class JanusGraphConnector
     private Gremlin.Net.Process.Traversal.GraphTraversalSource _g;
     private GremlinClient _client;
 
+
     public JanusGraphConnector(string host)
     {
         var serializer = new JanusGraphGraphSONMessageSerializer();
         _client = new GremlinClient(new GremlinServer(host, 8182), serializer);
 
     }
-    public Gremlin.Net.Process.Traversal.GraphTraversalSource traversal()
+    public GraphTraversalSource traversal()
     {
-        var g = Traversal().WithRemote(new DriverRemoteConnection(_client));
-        return g;
+        if (_g == null)
+        {
+            var g = Traversal().WithRemote(new DriverRemoteConnection(_client));
+            _g = g;
+        }
+        return _g;
     }
+
     public GremlinClient Client()
     {
         return _client;

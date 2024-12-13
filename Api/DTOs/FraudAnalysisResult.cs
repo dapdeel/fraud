c8227b6518caf29cb6e2e3f0ee53e3899d16a486
Alao Gbolahan Sodiq <98753363+Gbolahan-Alao@users.noreply.github.com>
2024-12-13 02:11:24 +0100

    added dummy analysis

---
+++ Api/DTOs/FraudAnalysisResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.DTOs
+{
+    public class FraudAnalysisResult
+    {
+        public double FraudScore { get; set; }
+        public double InfluentialScore { get; set; }
+        public double BlacklistScore { get; set; }
+        public double AnomalyScore { get; set; }
+    }
+}
